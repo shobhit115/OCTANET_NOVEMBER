7fc1bcbce31484c3f276ce318ac625020b675b3e
Shobhit Singh <112196213+shobhit115@users.noreply.github.com>
2024-11-24 20:12:30 +0530

    Add files via upload

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Logo from "@/components/ui/logo";
+import AuthBg from "@/public/images/auth-bg.svg";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <header className="absolute z-30 w-full">
+        <div className="mx-auto max-w-6xl px-4 sm:px-6">
+          <div className="flex h-16 items-center justify-between md:h-20">
+            {/* Site branding */}
+            <div className="mr-4 shrink-0">
+              <Logo />
+            </div>
+          </div>
+        </div>
+      </header>
+
+      <main className="relative flex grow">
+        <div
+          className="pointer-events-none absolute bottom-0 left-0 -translate-x-1/3"
+          aria-hidden="true"
+        >
+          <div className="h-80 w-80 rounded-full bg-gradient-to-tr from-blue-500 opacity-70 blur-[160px]"></div>
+        </div>
+
+        {/* Content */}
+        <div className="w-full">
+          <div className="flex h-full flex-col justify-center before:min-h-[4rem] before:flex-1 after:flex-1 md:before:min-h-[5rem]">
+            <div className="px-4 sm:px-6">
+              <div className="mx-auto w-full max-w-sm">
+                <div className="py-16 md:py-20">{children}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <>
+          {/* Right side */}
+          
+        </>
+      </main>
+    </>
+  );
+}
